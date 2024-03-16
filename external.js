742e3417c3485a843d7d2b1ac381cfabe4ae9a67
TheikdiSann <152377072+TheikdiSann@users.noreply.github.com>
2024-03-16 09:17:25 +0700

    Update external.js with playRound(), playGame() and scoreDisplay()

--- external.js
+++ external.js
@@ -2,25 +2,50 @@ let choices = ["rock", "paper", "scissor"]
 
 function getComputerChoice(){
     let randomNum =  Math.floor(Math.random() * 3) //random (0-1) * 3 so result will be 0-2 beacuse of Math.floor()
-    return choices[randomNum]
+    return randomNum
 }
 
 
 function getPlayerChoice(){
-    let playerChoice, isCorrectWord
+    let playerChoice 
     do {
         playerChoice = prompt("Enter a choice of Rock, Paper, Scissor")
-        isCorrectWord = gettingIncorrectInput(playerChoice)
-    } while (!playerChoice || !isCorrectWord);
-
-    return playerChoice
+        
+    } while (!playerChoice || choices.indexOf(playerChoice.toLowerCase()) < 0);
+    return choices.indexOf(playerChoice.toLowerCase())
     
 }
 
-function gettingIncorrectInput(playerChoice){
-   let isIncluded =  choices.filter((choice)=>choice === playerChoice.toLowerCase())
-    return isIncluded.length > 0 ? isIncluded[0] : ''
+/* playRound function (play one time)*/
+
+function playRound(computerChoice, playerChoice){
+    let result = playerChoice === computerChoice ? 
+                    "Tie" : Math.abs(playerChoice - computerChoice) === 2 ?
+                    playerChoice < computerChoice ? "Player wins": "Computer wins": playerChoice > computerChoice ? "Player wins" : "Computer wins"
+
+    console.log(`Player chooses ${choices[playerChoice]} and Computer Chooses ${choices[computerChoice]}\nSo, ${result}.`)
+    return result
 }
 
-console.log(getComputerChoice())
-console.log(getPlayerChoice())
+
+/* playGame function */
+
+function playGame(){
+    let score = []
+    for (let i = 0; i <5 ; i++){
+        score[i] = playRound(getComputerChoice(), getPlayerChoice())
+    }
+
+    
+   return score
+}
+
+console.log(scoreDisplay(playGame())) 
+
+function scoreDisplay(score){
+    let player = score.filter((value) => value === "Player wins").length
+    let computer = score.filter((value) => value === "Computer wins").length
+    let tie = score.filter((value) => value === "Tie").length
+
+    return `Total Score: \nPlayer: ${player}\nTie: ${tie}\nComputer: ${computer}`
+}
